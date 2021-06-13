51d22a3adf604c5eb81aa06647a86a2e11ea175c
Evgenii Gurin <264449@niuitmo.ru>
2021-06-13 16:06:18 +0300

    add basic structure

---
+++ src/cursor/cursor.c
@@ -0,0 +1,4 @@
+//
+// Created by user on 13.06.2021.
+//
+
---
+++ src/cursor/cursor.h
@@ -0,0 +1,8 @@
+//
+// Created by user on 13.06.2021.
+//
+
+#ifndef SPO_LAB_1_5_CURSOR_H
+#define SPO_LAB_1_5_CURSOR_H
+
+#endif //SPO_LAB_1_5_CURSOR_H
