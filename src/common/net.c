deaec4eb47205d5e688328c198a36d28263f2c9d
Evgenii Gurin <264449@niuitmo.ru>
2021-06-15 14:54:39 +0300

    update cursor

---
+++ src/common/net.c
@@ -0,0 +1,99 @@
+#include "net.h"
+
+int socket_open(int *connect_socket) {
+    printf(MESSAGE_SOCKET_OPEN);
+    *connect_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (connect_socket < 0) {
+        printf(ERROR_MESSAGE_SOCKET_OPEN);
+        perror(ERROR_MESSAGE_SOCKET_OPEN);
+        PRINTLN;
+        return ERROR_SOCKET_OPEN;
+    }
+    printf(MESSAGE_OK);
+    PRINTLN;
+    return 0;
+}
+
+int socket_connect (const int *client_socket, struct sockaddr_in *server_address) {
+    printf(MESSAGE_SOCKET_CONNECT);
+    if (connect(*client_socket, (struct sockaddr *) server_address, sizeof(struct sockaddr_in)) == -1) {
+        printf(ERROR_MESSAGE_SOCKET_CONNECT);
+        perror(ERROR_MESSAGE_SOCKET_CONNECT);
+        PRINTLN;
+        return ERROR_CONNECT;
+    }
+    printf(MESSAGE_OK);
+    PRINTLN;
+    return 0;
+}
+
+
+int socket_set_name(const int *connect_socket, int *reuse) {
+    printf(MESSAGE_SOCKET_SET_NAME);
+    if (setsockopt(*connect_socket, SOL_SOCKET, SO_REUSEADDR, (const char *) reuse, sizeof(int)) == -1) {
+        printf(ERROR_MESSAGE_SOCKET_SET_NAME);
+        perror(ERROR_MESSAGE_SOCKET_SET_NAME);
+        PRINTLN;
+        return ERROR_SOCKET_SET_NAME;
+    }
+    printf(MESSAGE_OK);
+    PRINTLN;
+    return 0;
+}
+
+int socket_bind(const int *connect_socket, struct sockaddr_in *server_address) {
+    printf(MESSAGE_SOCKET_BIND);
+    if (bind(*connect_socket, (struct sockaddr *) server_address, sizeof(struct sockaddr_in)) == -1) {
+        printf(ERROR_MESSAGE_SOCKET_BIND);
+        perror(ERROR_MESSAGE_SOCKET_BIND);
+        PRINTLN;
+        return ERROR_SOCKET_BIND;
+    }
+    printf(MESSAGE_OK);
+    PRINTLN;
+    return 0;
+}
+
+int socket_listen(const int *connect_socket, int max_clients_amount) {
+    printf(MESSAGE_SOCKET_LISTEN);
+    if (listen(*connect_socket, max_clients_amount) == -1) {
+        printf(ERROR_MESSAGE_SOCKET_LISTEN);
+        perror(ERROR_MESSAGE_SOCKET_LISTEN);
+        PRINTLN;
+        return ERROR_SOCKET_LISTEN;
+    }
+    printf(MESSAGE_OK);
+    PRINTLN;
+    return 0;
+}
+
+size_t init_connection(int *connect_socket, struct sockaddr_in *server_address, long port, int *reuse, int max_clients_amount) {
+    int return_code = 0;
+
+    return_code = socket_open(connect_socket);
+    if (return_code != 0) return return_code;
+
+    server_address->sin_family = AF_INET;
+    server_address->sin_port = htons(port);
+    server_address->sin_addr.s_addr = htonl(INADDR_ANY);
+
+    return_code = socket_set_name(connect_socket, reuse);
+    if (return_code != 0) return return_code;
+    return_code = socket_bind(connect_socket, server_address);
+    if (return_code != 0) return return_code;
+    return_code = socket_listen(connect_socket, max_clients_amount);
+    if (return_code != 0) return return_code;
+    printf(MESSAGE_INIT_FINISH);
+    PRINTLN;
+
+    return 0;
+}
+
+bool validate_ip(char * address) {
+    struct sockaddr_in sa;
+    return inet_pton(AF_INET, address, &(sa.sin_addr)) != 0;
+}
+
+bool validate_port(int port) {
+    return port < 65535 && port > 1;
+}
--- src/common/net.h
+++ src/common/net.h
@@ -7,4 +7,61 @@
 #define COMMAND_CODE_DELETE 3
 #define COMMAND_CODE_INSERT 4
 
+#include "stdio.h"
+#include "stdbool.h"
+
+#ifdef __WIN32__
+#include "windows.h"
+#include "ws2tcpip.h"
+#define SHUT_RDWR SD_BOTH
+#else
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#endif
+
+size_t init_connection(int *connect_socket, struct sockaddr_in *server_address, long port, int *reuse, int max_clients_amount);
+
+int socket_open(int *connect_socket);
+int socket_connect (const int *client_socket, struct sockaddr_in *server_address);
+int socket_check_connect(const int *socket);
+void socket_close(const int *socket);
+int socket_set_name(const int *connect_socket, int *reuse);
+int socket_bind(const int *connect_socket, struct sockaddr_in *server_address);
+int socket_listen(const int *connect_socket, int max_clients_amount);
+
+bool validate_ip(char * address);
+bool validate_port(int port);
+
+#define PRINTLN printf("\n")
+
+#define ERROR_MESSAGE_CLIENTS_OVERFLOW "Amount of clients is overflowed"
+#define ERROR_MESSAGE_SOCKET_ACCEPT "Socket accept() failed"
+#define ERROR_MESSAGE_CONNECT_NEW_CLIENT "New client cant be connected"
+#define ERROR_MESSAGE_UNDEFINED "Undefined error"
+#define ERROR_MESSAGE_SOCKET_OPEN "Open socket failed"
+#define ERROR_MESSAGE_SOCKET_CONNECT "Connect socket failed"
+#define ERROR_MESSAGE_SOCKET_LISTEN "Listen socket failed"
+#define ERROR_MESSAGE_SOCKET_SET_NAME "Set socket name failed"
+#define ERROR_MESSAGE_SOCKET_BIND "Bind socket failed"
+
+#define MESSAGE_CLIENTS_FULL "Amount of client is full"
+#define MESSAGE_SOCKET_OPEN "Open socket..."
+#define MESSAGE_SOCKET_CONNECT "Connect socket..."
+#define MESSAGE_SOCKET_LISTEN "Listen socket..."
+#define MESSAGE_SOCKET_SET_NAME "Set socket name..."
+#define MESSAGE_SOCKET_BIND "Bind socket..."
+#define MESSAGE_SOCKET_CLOSE "Close socket"
+#define MESSAGE_INIT_FINISH "Init finished"
+#define MESSAGE_OK "OK"
+
+#define ERROR_SOCKET_ACCEPT 3
+#define ERROR_SOCKET_OPEN 4
+#define ERROR_SOCKET_SET_NAME 5
+#define ERROR_SOCKET_BIND  6
+#define ERROR_SOCKET_LISTEN 7
+#define ERROR_CONNECT 9
+
 #endif //SPO_LAB_1_5_NET_H
